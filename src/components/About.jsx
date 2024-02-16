e5c825c4e9f0a72fa5bd404a2cd8798ecbce3ecb
alankritvyas21 <av3h.vyas@gmail.com>
2024-02-16 23:03:47 +0530

    box-shadows

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -60,7 +60,7 @@ const About = () => {
               <div className="about__box">
                 <i class="bx bx-briefcase-alt about__icon"></i>
                 <h3 className="about__title">Experience</h3>
-                <span className="about__subtitle">9 months</span>
+                <span className="about__subtitle">1 Year</span>
               </div>
 
               <div className="about__box">
