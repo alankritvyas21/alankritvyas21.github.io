e127e42a43c95d4604dfd7b7fb30d9f76decf71f
alankritvyas21 <av3h.vyas@gmail.com>
2024-07-13 22:43:02 +0530

    Skills in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,59 +15,28 @@ const Header = () => {
         <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a
-                href="#home"
-                className="nav__link active-link"
-                onClick={() => {
-                  setToggle(false);
-                }}
-              >
-                <i className="uil uil-estate nav__icons"></i>Home
-              </a>
+              <a href="#home" className="nav__link active-link" onClick={() => {setToggle(false);}}>
+                <i className="uil uil-estate nav__icons"></i>Home</a>
             </li>
             <li className="nav__item">
-              <a
-                href="#about"
-                className="nav__link active-link"
-                onClick={() => {
-                  setToggle(false);
-                }}
-              >
-                <i className="uil uil-user nav__icons"></i>About
-              </a>
+              <a href="#about" className="nav__link active-link" onClick={() => {setToggle(false);}}>
+                <i className="uil uil-user nav__icons"></i>About</a>
             </li>
             <li className="nav__item">
-              <a
-                href="#resume"
-                className="nav__link active-link"
-                onClick={() => {
-                  setToggle(false);
-                }}
-              >
-                <i className="uil uil-file-alt nav__icons"></i>Resume
-              </a>
+              <a href="#resume" className="nav__link active-link" onClick={() => {setToggle(false);}}>
+                <i className="uil uil-file-alt nav__icons"></i>Resume</a>
             </li>
             <li className="nav__item">
-              <a
-                href="#projects"
-                className="nav__link active-link"
-                onClick={() => {
-                  setToggle(false);
-                }}
-              >
-                <i className="uil uil-scenery nav__icons"></i>Projects
-              </a>
+              <a href="#skills" className="nav__link active-link" onClick={() => {setToggle(false);}}>
+                <i className="uil uil-chart nav__icons"></i>Skills</a>
             </li>
             <li className="nav__item">
-              <a
-                href="#contact"
-                className="nav__link active-link"
-                onClick={() => {
-                  setToggle(false);
-                }}
-              >
-                <i className="uil uil-message nav__icons"></i>Contact
-              </a>
+              <a href="#projects" className="nav__link active-link" onClick={() => {setToggle(false);}}>
+                <i className="uil uil-scenery nav__icons"></i>Projects</a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link active-link" onClick={() => {setToggle(false);}}>
+                <i className="uil uil-message nav__icons"></i>Contact</a>
             </li>
             {/* <li className="nav__item">
               <label className="toggle-switch">
