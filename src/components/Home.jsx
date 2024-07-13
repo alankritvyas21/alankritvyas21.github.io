040589110a03ee4056ad8201aea053bffe8ca313
alankritvyas21 <av3h.vyas@gmail.com>
2024-07-13 22:56:34 +0530

    Home Animations Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,19 +11,10 @@ const Home = () => {
         <div className="home__container container grid">
           <div className="home__content grid">
             <div className="home__img">
-              <img
-                src={mainImg}
-                data-aos="fade-left"
-                data-aos-delay="300"
-                alt=""
-                className="home__img"
-              />
+              <img src={mainImg} data-aos="fade-left" data-aos-delay="300" alt="" className="home__img"/>
             </div>
             <div
-              className="container_mouse"
-              data-aos="fade-right"
-              data-aos-delay="300"
-            >
+              className="container_mouse" data-aos="fade-right" data-aos-delay="300">
               <span className="mouse-btn">
                 <span className="mouse-scroll"></span>
               </span>
@@ -39,59 +30,30 @@ const Home = () => {
                 Jr. Software Engineer at Dusane Infotech India Pvt Ltd
               </p>
 
-              <div
-                className="home__social"
-                data-aos="fade-up"
-                data-aos-delay="500"
-              >
-                <a
-                  href="https://bento.me/alankritvyas"
-                  className="bento"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+              <div className="home__social" data-aos="fade-up" data-aos-delay="500" >
+                <a href="https://bento.me/alankritvyas" className="bento" target="_blank" rel="noopener noreferrer" >
                   <i className="uil uil-user-circle"></i>
                 </a>
 
-                <a
-                  href="mailto:av3h.vyas@gmail.com"
-                  className="email"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <a href="mailto:av3h.vyas@gmail.com" className="email" target="_blank" rel="noopener noreferrer" >
                   <i className="uil uil-envelope-alt"></i>
                 </a>
 
-                <a
-                  href="https://www.instagram.com/alankritvyas/"
-                  className="instagram"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <a href="https://www.instagram.com/alankritvyas/" className="instagram" target="_blank" rel="noopener noreferrer" >
                   <i className="uil uil-instagram"></i>
                 </a>
 
-                <a
-                  href="https://www.linkedin.com/in/alankrit-vyas-667063190/"
-                  className="linkedin"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <a href="https://www.linkedin.com/in/alankrit-vyas-667063190/" className="linkedin" target="_blank" rel="noopener noreferrer" >
                   <i className="uil uil-linkedin"></i>
                 </a>
 
-                <a
-                  href="https://github.com/alankritvyas21"
-                  className="github"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <a href="https://github.com/alankritvyas21" className="github" target="_blank" rel="noopener noreferrer" >
                   <i className="uil uil-github"></i>
                 </a>
               </div>
 
               <a href="#contact">
-                <button className="button button--flex">
+                <button className="button button--flex" data-aos="fade-up" data-aos-delay="500">
                   Say Hello <i className="uil uil-message home__icons"></i>
                 </button>
               </a>
